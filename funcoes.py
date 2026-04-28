6bf44407fa85b042f4d5c9093eb4e0ed5c33e6db
Pierods444 <piero.csora@gmail.com>
2026-04-28 13:46:48 -0300

    exercicios 1 e 2 -Guardar dado e remover dado

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,17 @@ def rolar_dados (numero_de_dados):
         lista.append(random.randint(1,6))
         i += 1
     return lista
+
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    novo_rolados = []
+    i = 0
+
+    while i < len(dados_rolados):
+        if i != dado_para_guardar:
+            novo_rolados.append(dados_rolados[i])
+        i = i + 1
+
+    dados_no_estoque.append(dados_rolados[dado_para_guardar])
+
+    return [novo_rolados, dados_no_estoque]
